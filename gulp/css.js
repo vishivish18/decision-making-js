26d8dbd60318f1053b9f28b74aeb543bfe0285c9
Vishal Ranjan <vishivish18@gmail.com>
2016-08-29 13:28:43 +0530

    adding gulp-sass and compiling css through sass

--- gulp/css.js
+++ gulp/css.js
@@ -1,12 +1,12 @@
-var gulp = require('gulp')
-var stylus = require('gulp-stylus')
+var gulp = require('gulp');
+var sass = require('gulp-sass');
 
-gulp.task('css', function() {
-    return gulp.src('css/**/*.styl')
-        .pipe(stylus())
-        .pipe(gulp.dest('assets'))
-})
+gulp.task('css', function () {
+  return gulp.src(['public/assets/sass/*.scss'])
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('public/dist'));
+});
 
-gulp.task('watch:css', ['css'], function() {
-    gulp.watch('css/**/*.styl', ['css'])
-})
+gulp.task('watch:css',['css'], function () {
+  gulp.watch('/sass/**/*.scss', ['css']);
+});
